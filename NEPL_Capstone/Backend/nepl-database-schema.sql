810bb1e85c8f6edee3a799ad8f970cde098ff12a
erico07181 <erico@salesforce.com>
2022-07-22 13:02:19 -0700

    Added managers table

--- NEPL_Capstone/Backend/nepl-database-schema.sql
+++ NEPL_Capstone/Backend/nepl-database-schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE
   users (
     id SERIAL PRIMARY KEY,
-    password TEXT NOT NULL,
+    password `TEXT NOT NULL`,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE CHECK (POSITION('@' IN email) > 1),
@@ -25,6 +25,14 @@ CREATE TABLE
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     module_id INTEGER NOT NULL REFERENCES modules(id) ON DELETE CASCADE
   );
+
+CREATE TABLE manager (
+  id          SERIAL PRIMARY KEY,
+  user_id     INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  token       TEXT NOT NULL,
+  company     TEXT NOT NULL,
+  usersInPod  INTEGER[]
+);
 
 INSERT INTO
   modules (name, category, steps)
