2cab4ef2b58d3de6fe19527e84b2282b0a0f12fc
Paul Franco <paul.franco@paulfra-ltmkbcz.internal.salesforce.com>
2022-07-26 11:43:49 -0700

    Register Complete, Login Complete

--- NEPL_Capstone/Frontend/src/components/Login/Login.jsx
+++ NEPL_Capstone/Frontend/src/components/Login/Login.jsx
@@ -11,8 +11,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -57,6 +55,7 @@ function Login() {
             required
             fullWidth
             label="Enter your email"
+            type="email"
             name="email"
             autoFocus
             onChange={handleOnInputChange}
