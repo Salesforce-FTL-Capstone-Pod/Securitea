90f269825701efc9a31ae077f70282fbcc877740
Lucas Ribeiro Vidal <lribeirovidal@salesforce.com>
2022-07-27 10:21:23 -0700

    fixed part of the screen glitch

--- NEPL_Capstone/Frontend/src/components/App/App.jsx
+++ NEPL_Capstone/Frontend/src/components/App/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 //Components
-import Landing from '../Landing/Landing'
-import Login from '../Login/Login'
-import Register from "../Register/Register"
-import UserDashboard from '../UserDashboard/UserDashboard'
-import ModulePage from '../ModulePage/ModulePage'
+import Landing from "../Landing/Landing";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
+import UserDashboard from "../UserDashboard/UserDashboard";
+import ModulePage from "../ModulePage/ModulePage";
 //Contexts
-import { AuthContextProvider } from "../../contexts/auth"
-import { useAuthContext } from "../../contexts/auth"
+import { AuthContextProvider } from "../../contexts/auth";
+import { useAuthContext } from "../../contexts/auth";
 
 function App({}) {
-  return (
-    <AuthContextProvider>
-      <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/UserDashboard/" element={<UserDashboard />} />
-            <Route path="/ModulePage" element={<ModulePage /> } />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </AuthContextProvider>
-  );
+	return (
+		<AuthContextProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Landing />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/UserDashboard/" element={<UserDashboard />} />
+					<Route path="/ModulePage" element={<ModulePage />} />
+				</Routes>
+			</BrowserRouter>
+		</AuthContextProvider>
+	);
 }
 
-export default App
+export default App;
--- NEPL_Capstone/Frontend/src/components/Landing/Landing.jsx
+++ NEPL_Capstone/Frontend/src/components/Landing/Landing.jsx
@@ -1,185 +1,175 @@
 import * as React from "react";
 import {
-  Button,
-  Grid,
-  Container,
-  Box,
-  AppBar,
-  Toolbar,
-  Typography,
-  Stack,
+	Button,
+	Grid,
+	Container,
+	Box,
+	AppBar,
+	Toolbar,
+	Typography,
+	Stack,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import "./Landing.css";
 
 export default function Landing() {
-  return (
-    <div className="container">
-      <Navbar />
-      <Hero />
-      <SubHero />
-    </div>
-  );
+	return (
+		<>
+			<Navbar />
+			<Hero />
+			<SubHero />
+		</>
+	);
 }
 
 //Navbar function for the landing page
 export function Navbar() {
-  return (
-    <Container>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="fixed" style={{ background: "#0E131F" }}>
-          <Toolbar style={{ height: "40px" }}>
-            <Typography
-              variant="h6"
-              color="inherit"
-              sx={{ flexGrow: 1 }}
-              to="/"
-              component={Link}
-              style={{ textDecoration: "none" }}
-            >
-              SecuriTEA
-            </Typography>
-            <Stack direction="row" spacing={3}>
-              <Button color="inherit" to="/" component={Link}>
-                Home
-              </Button>
-              <Button color="inherit" to="/modules" component={Link}>
-                Modules
-              </Button>
-              <Button color="inherit" to="/resources" component={Link}>
-                Resources
-              </Button>
-              <Button color="inherit" to="/contant us" component={Link}>
-                Contact Us
-              </Button>
-            </Stack>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </Container>
-  );
+	return (
+		<>
+			<AppBar style={{ background: "#0E131F" }} position="sticky">
+				<Toolbar style={{ height: "40px" }}>
+					<Typography
+						variant="h6"
+						color="inherit"
+						sx={{ flexGrow: 1 }}
+						to="/"
+						component={Link}
+						style={{ textDecoration: "none" }}
+					>
+						SecuriTEA
+					</Typography>
+					<Stack direction="row" spacing={3}>
+						<Button color="inherit" to="/" component={Link}>
+							Home
+						</Button>
+						<Button color="inherit" to="/modules" component={Link}>
+							Modules
+						</Button>
+						<Button color="inherit" to="/resources" component={Link}>
+							Resources
+						</Button>
+						<Button color="inherit" to="/contant us" component={Link}>
+							Contact Us
+						</Button>
+					</Stack>
+				</Toolbar>
+			</AppBar>
+		</>
+	);
 }
 
 // Hero container for the landing page
 export function Hero() {
-  return (
-    <Stack direction="column" spacing={3}>
-      <Typography component={"span"}>
-        <h1
-          style={{
-            position: "fixed",
-            width: "540px",
-            height: "16px",
-            left: "860px",
-            top: "90px",
-            fontSize: "18px",
-          }}
-        >
-          Pellentesque sem elit, aliquet in tempor ac, lacinia vel ipsum.
-        </h1>
-        <p
-          style={{
-            position: "fixed",
-            width: "550px",
-            height: "28px",
-            left: "860px",
-            top: "150px",
-            fontSize: "12px",
-          }}
-        >
-          Sed blandit orci ut purus vestibulum tempor. Quisque sit amet faucibus
-          mi. Ut nisi metus, porttitor eu efficitur ut, vestibulum sed sapien.
-          Pellentesque sem elit, aliquet in tempor ac, lacinia vel ipsum. Donec
-          dignissim, eros nec sodales consectetur, tellus tellus vulputate eros
-        </p>
-      </Typography>
-      <Button
-        to="/register"
-        component={Link}
-        style={{
-          position: "fixed",
-          width: "127px",
-          height: "50px",
-          left: "1010px",
-          top: "232px",
-          fontSize: "10px",
-          background: "#A39EDA",
-          color: "#FFFFFF",
-        }}
-      >
-        Sign Up
-      </Button>
-      <Typography
-        component={"span"}
-        style={{
-          position: "fixed",
-          width: "150px",
-          height: "50px",
-          left: "1010px",
-          top: "300px",
-          fontSize: "12px",
-        }}
-      >
-        <p>
-          Have an account?
-          <Link style={{ textDecoration: "none" }} to="/login">
-            Sign In
-          </Link>
-        </p>
-      </Typography>
-    </Stack>
-  );
+	return (
+		<>
+			<Container
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					backgroundColor: "white",
+				}}
+				maxWidth={false}
+			>
+				<h1
+					style={{
+						width: "540px",
+						height: "16px",
+						fontSize: "18px",
+					}}
+				>
+					Pellentesque sem elit, aliquet in tempor ac, lacinia vel ipsum.
+				</h1>
+				<p
+					style={{
+						width: "550px",
+						height: "28px",
+						fontSize: "12px",
+					}}
+				>
+					Sed blandit orci ut purus vestibulum tempor. Quisque sit amet faucibus
+					mi. Ut nisi metus, porttitor eu efficitur ut, vestibulum sed sapien.
+					Pellentesque sem elit, aliquet in tempor ac, lacinia vel ipsum. Donec
+					dignissim, eros nec sodales consectetur, tellus tellus vulputate eros
+				</p>
+
+				<Button
+					to="/register"
+					component={Link}
+					style={{
+						width: "127px",
+						height: "50px",
+						fontSize: "10px",
+						background: "#A39EDA",
+						color: "#FFFFFF",
+					}}
+				>
+					Sign Up
+				</Button>
+
+				<Typography
+					component={"span"}
+					style={{
+						width: "150px",
+						height: "50px",
+						fontSize: "12px",
+					}}
+				>
+					<p>
+						Have an account?
+						<Link style={{ textDecoration: "none" }} to="/login">
+							Sign In
+						</Link>
+					</p>
+				</Typography>
+			</Container>
+		</>
+	);
 }
 //SubHero for the landing page
 export function SubHero() {
-  return (
-    <Box>
-      <Grid
-        container
-        spacing={7}
-        style={{
-          backgroundColor: "#D3CFE2",
-          position: "fixed",
-          width: "100%",
-          height: "500px",
-          right: "0px",
-          top: "470px",
-        }}
-        direction="row"
-      ></Grid>
-      <Typography component={"span"}>
-        <h1
-          style={{
-            position: "fixed",
-            width: "230px",
-            height: "50px",
-            left: "598px",
-            top: "430px",
-            fontSize: "18px",
-            color: "black",
-          }}
-        >
-          Current Modules Available
-        </h1>
-      </Typography>
+	return (
+		<>
+			<Grid
+				container
+				spacing={7}
+				style={{
+					backgroundColor: "#D3CFE2",
+					width: "100%",
+					height: "500px",
+					flexDirection: "column",
+					margin: "0px",
+				}}
+				direction="row"
+			>
+				<Typography component={"span"}>
+					<h1
+						style={{
+							width: "230px",
+							height: "50px",
+							fontSize: "18px",
+							color: "black",
+						}}
+					>
+						Current Modules Available
+					</h1>
+				</Typography>
 
-      <Button
-        to="/modules"
-        component={Link}
-        style={{
-          position: "fixed",
-          width: "230px",
-          height: "50px",
-          left: "598px",
-          top: "780px",
-          fontSize: "10px",
-          background: "#0E131F",
-          color: "white",
-          boarderRadius: "100px",
-        }}
-      >
-        VIEW ALL MODULES
-      </Button>
-    </Box>
-  );
+				<Button
+					to="/modules"
+					component={Link}
+					style={{
+						width: "230px",
+						height: "50px",
+
+						fontSize: "10px",
+						background: "#0E131F",
+						color: "white",
+						boarderRadius: "100px",
+					}}
+				>
+					VIEW ALL MODULES
+				</Button>
+			</Grid>
+		</>
+	);
 }
