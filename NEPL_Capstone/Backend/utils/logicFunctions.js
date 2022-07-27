c6108722b4fa171bfcfd42d85c573fb6a8bc0986
erico07181 <erico@salesforce.com>
2022-07-27 14:34:01 -0700

    Fixed random string gen

--- NEPL_Capstone/Backend/utils/logicFunctions.js
+++ NEPL_Capstone/Backend/utils/logicFunctions.js
@@ -1,4 +1,4 @@
-function generateRandomString(length) {
+function randomStringGenerator(length) {
   var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   var charactersLen = chars.length;
   for (var i = 0; i < length; i++) {
@@ -22,6 +22,6 @@ function createToken(company, first_name, last_name) {
 }
 
 module.exports = {
-  generateRandomString,
+  randomStringGenerator,
   createToken,
 };
