6cb8a736d8dcd2d85d84f72ec1fb37151a627a82
Lucas Ribeiro Vidal <lribeirovidal@salesforce.com>
2022-07-27 16:24:06 -0700

    added navbar to modules page

--- NEPL_Capstone/Frontend/src/components/ModulePage/ModulePage.jsx
+++ NEPL_Capstone/Frontend/src/components/ModulePage/ModulePage.jsx
@@ -1,56 +1,77 @@
-import React from 'react'
-import "./ModulePage.css"
-import Logo from "../../assets/Logo.svg"
-import { Link } from "react-router-dom"
-import { css, Button, Text, Container, Card, Row, Col, Spacer } from '@nextui-org/react'
+import React from "react";
+import "./ModulePage.css";
+import Logo from "../../assets/Logo.svg";
+import { Link } from "react-router-dom";
+import { css, Text, Card, Row, Col, Spacer } from "@nextui-org/react";
+import {
+	Button,
+	Grid,
+	Container,
+	Box,
+	AppBar,
+	Toolbar,
+	Typography,
+	Stack,
+} from "@mui/material";
 
 function ModulePage() {
-
-  return (
-    <div className='ModulePage'>
-      <div className='HeroBackground'>
-        <Navbar/>
-        
-        <div className='Hero'>
-          <Text h2 size={15} color="white" css={{ m: 0 }}>
-            Test
-          </Text>
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<Container maxWidth={false} disableGutters>
+			<Navbar />
+		</Container>
+	);
 }
- 
-export default ModulePage
 
-
+export default ModulePage;
 
 const navLinks = [
-  { label: "Home", path: `/` },
-  { label: "Modules", path: `/modules` },
-  { label: "Resources", path: `/resources` },
-  { label: "Contact Us", path: `/contactus` },
-]
+	{ label: "Home", path: `/` },
+	{ label: "Modules", path: `/modules` },
+	{ label: "Resources", path: `/resources` },
+	{ label: "Contact Us", path: `/contactus` },
+];
 function Navbar() {
-  return (
-    <nav className="Navbar">
-      <div className="content">
-        <div className="logo">
-          <Link to="/">
-            <img src={Logo} alt="logo" />
-          </Link>
-        </div>
-
-        <ul className="links">
-          {navLinks.map((link) => (
-            <li key={link.path}>
-              <Link to={link.path}>{link.label}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </nav>
-  )
+	return (
+		<AppBar style={{ background: "#0E131F" }} position="sticky">
+			<Toolbar style={{ height: "40px", justifyContent: "space-between" }}>
+				<div style={{ display: "flex" }}>
+					<img src={Logo}></img>
+					<Stack direction="row" spacing={3}>
+						<Button color="inherit" to="/" component={Link}>
+							Home
+						</Button>
+						<Button color="inherit" to="/ModulePage" component={Link}>
+							Modules
+						</Button>
+						<Button color="inherit" to="/resources" component={Link}>
+							Resources
+						</Button>
+						<Button color="inherit" to="/contant us" component={Link}>
+							Contact Us
+						</Button>
+					</Stack>
+				</div>
+				<div>
+					<Button
+						color="inherit"
+						to="/Login"
+						component={Link}
+						sx={{ backgroundColor: "#A39EDA", marginRight: "1vw" }}
+					>
+						Login
+					</Button>
+					<Button
+						color="inherit"
+						to="/Register"
+						component={Link}
+						sx={{ backgroundColor: "#A39EDA" }}
+					>
+						Register
+					</Button>
+				</div>
+			</Toolbar>
+		</AppBar>
+	);
 }
 
-//Ex using theme        <Text css={{ background: '$black' }}>NextUI colors</Text> 
+//Ex using theme        <Text css={{ background: '$black' }}>NextUI colors</Text>
