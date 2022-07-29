a27d1225edc00f0f05e3c4cb6a1ab514339ccd5a
Lucas Ribeiro Vidal <lribeirovidal@salesforce.com>
2022-07-29 11:34:29 -0700

    fix

--- NEPL_Capstone/Frontend/src/components/Navbar/Navbar.jsx
+++ NEPL_Capstone/Frontend/src/components/Navbar/Navbar.jsx
@@ -6,10 +6,10 @@ import { useAuthContext } from "../../contexts/auth";
 import * as color from "../../assets/colorPalette.jsx";
 
 export default function Navbar() {
-	const { user, handleLogout } = useAuthContext()
+	const { user, handleLogout } = useAuthContext();
 	return (
-		<AppBar style={{ background: color.richBlackFogra }} position="sticky">
-			<Toolbar style={{ height: "40px", justifyContent: "space-between" }}>
+		<AppBar sx={{ background: color.richBlackFogra }} position="sticky">
+			<Toolbar sx={{ height: "4vw", justifyContent: "space-between" }}>
 				<div style={{ display: "flex" }}>
 					<Link
 						style={{
@@ -42,47 +42,48 @@ export default function Navbar() {
 				</div>
 				<div>
 					{user?.email ? (
-					<>
-						<Button
-							variant="contained"
-							to="/Register"
-							component={Link}
-							sx={{
-								backgroundColor: color.languidLavender,
-								color: color.richBlackFogra,
-							}}
-							onClick={handleLogout}
-						>
-							Log out
-						</Button>
-					</>
-					):(
-					<>
-						<Button
-							color="inherit"
-							to="/Login"
-							component={Link}
-							sx={{
-								border: `2px solid ${color.languidLavender}`,
-								marginRight: "1vw",
-								paddingLeft: "1vw",
-								paddingRight: "1vw",
-							}}
-						>
-							Login
-						</Button>
-						<Button
-							variant="contained"
-							to="/Register"
-							component={Link}
-							sx={{
-								backgroundColor: color.languidLavender,
-								color: color.richBlackFogra,
-							}}
-						>
-							Sign Up
-						</Button>
-					</>)}
+						<>
+							<Button
+								variant="contained"
+								to="/Register"
+								component={Link}
+								sx={{
+									backgroundColor: color.languidLavender,
+									color: color.richBlackFogra,
+								}}
+								onClick={handleLogout}
+							>
+								Log out
+							</Button>
+						</>
+					) : (
+						<>
+							<Button
+								color="inherit"
+								to="/Login"
+								component={Link}
+								sx={{
+									border: `2px solid ${color.languidLavender}`,
+									marginRight: "1vw",
+									paddingLeft: "1vw",
+									paddingRight: "1vw",
+								}}
+							>
+								Login
+							</Button>
+							<Button
+								variant="contained"
+								to="/Register"
+								component={Link}
+								sx={{
+									backgroundColor: color.languidLavender,
+									color: color.richBlackFogra,
+								}}
+							>
+								Sign Up
+							</Button>
+						</>
+					)}
 				</div>
 			</Toolbar>
 		</AppBar>
