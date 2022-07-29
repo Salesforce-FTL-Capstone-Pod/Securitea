d53d468488b67b72928ece5d5e59b8d65ca52fbd
erico07181 <erico@salesforce.com>
2022-07-29 09:51:10 -0700

    created safety tip module page

--- NEPL_Capstone/Frontend/src/components/App/App.jsx
+++ NEPL_Capstone/Frontend/src/components/App/App.jsx
@@ -8,6 +8,7 @@ import Login from "../Login/Login";
 import Register from "../Register/Register";
 import UserDashboard from "../UserDashboard/UserDashboard";
 import ModulePagePhishing from "../ModulePage/ModulePagePhishing";
+import ModulePageTips from "../ModulePage/ModulePageTips";
 import Modules from "../Modules/Modules";
 import * as color from "../../assets/colorPalette";
 //Libraries
@@ -18,45 +19,46 @@ import { AuthContextProvider } from "../../contexts/auth";
 import { useAuthContext } from "../../contexts/auth";
 
 export default function AppContainer() {
-	return (
-		<NextUIProvider theme={theme}>
-			<AuthContextProvider>
-				<App />
-			</AuthContextProvider>
-		</NextUIProvider>
-	);
+  return (
+    <NextUIProvider theme={theme}>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </NextUIProvider>
+  );
 }
 function App() {
-	return (
-		<div className="App">
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Landing />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/register" element={<Register />} />
-					<Route path="/UserDashboard/" element={<UserDashboard />} />
-					<Route path="/ModulePhishing" element={<ModulePagePhishing />} />
-					<Route path="/Modules" element={<Modules />} />
-				</Routes>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/UserDashboard/" element={<UserDashboard />} />
+          <Route path="/ModulePhishing" element={<ModulePagePhishing />} />
+          <Route path="/ModuleTips" element={<ModulePageTips />} />
+          <Route path="/Modules" element={<Modules />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 const theme = createTheme({
-	type: "light",
-	theme: {
-		colors: {
-			platinum: "#ECEBEB",
-			black: "#0E131F",
-			lightpurple: "#D3CFE2",
-			medpurple: "#BEBAE0",
-			darkpurple: "3E3B62",
-		},
-		space: {},
-		fonts: {
-			wee: "Roboto",
-			mono: "Open Sans",
-		},
-	},
+  type: "light",
+  theme: {
+    colors: {
+      platinum: "#ECEBEB",
+      black: "#0E131F",
+      lightpurple: "#D3CFE2",
+      medpurple: "#BEBAE0",
+      darkpurple: "3E3B62",
+    },
+    space: {},
+    fonts: {
+      wee: "Roboto",
+      mono: "Open Sans",
+    },
+  },
 });
