114a8f22f0c4a50765ed15b673fc6395ebe1606a
Lucas Ribeiro Vidal <lribeirovidal@salesforce.com>
2022-08-02 15:26:18 -0700

    contact us page

--- NEPL_Capstone/Frontend/src/components/App/App.jsx
+++ NEPL_Capstone/Frontend/src/components/App/App.jsx
@@ -8,6 +8,7 @@ import Landing from "../Landing/Landing";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
 import UserDashboard from "../UserDashboard/UserDashboard";
+import ContactUs from "../ContactUs/ContactUs";
 
 import ModulePage from "../ModulePage/ModulePagePhishing";
 import ResourcePage from "../ResourcePage/ResourcePage";
@@ -25,50 +26,50 @@ import { NextUIProvider, createTheme } from "@nextui-org/react";
 import { AuthContextProvider } from "../../contexts/auth";
 import { useAuthContext } from "../../contexts/auth";
 
-
 export default function AppContainer() {
-  return (
-    <NextUIProvider theme={theme}>
-      <AuthContextProvider>
-        <App />
-      </AuthContextProvider>
-    </NextUIProvider>
-  );
+	return (
+		<NextUIProvider theme={theme}>
+			<AuthContextProvider>
+				<App />
+			</AuthContextProvider>
+		</NextUIProvider>
+	);
 }
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/UserDashboard/" element={<UserDashboard />} />
-          <Route path="/ModulePhishing" element={<ModulePagePhishing />} />
-          <Route path="/ModuleTips" element={<ModulePageTips />} />
-          <Route path="/Modules" element={<Modules />} />
-          <Route path="/Resources" element={<ResourcePage/>}/>
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Landing />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/UserDashboard/" element={<UserDashboard />} />
+					<Route path="/ModulePhishing" element={<ModulePagePhishing />} />
+					<Route path="/ModuleTips" element={<ModulePageTips />} />
+					<Route path="/Modules" element={<Modules />} />
+					<Route path="/Resources" element={<ResourcePage />} />
+					<Route path="/Contact-Us" element={<ContactUs />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 const theme = createTheme({
-  type: "light",
-  theme: {
-    colors: {
-      platinum: color.platinum,
-      black: color.richBlackFogra,
-      lightpurple: color.languidLavender,
-      medpurple: color.blueBell,
-      darkpurple: color.maximumBluePurple,
-    },
-    space: {},
-    fonts: {
-      wee: "Roboto",
-      mono: "Open Sans",
-    },
-  },
+	type: "light",
+	theme: {
+		colors: {
+			platinum: color.platinum,
+			black: color.richBlackFogra,
+			lightpurple: color.languidLavender,
+			medpurple: color.blueBell,
+			darkpurple: color.maximumBluePurple,
+		},
+		space: {},
+		fonts: {
+			wee: "Roboto",
+			mono: "Open Sans",
+		},
+	},
 });
