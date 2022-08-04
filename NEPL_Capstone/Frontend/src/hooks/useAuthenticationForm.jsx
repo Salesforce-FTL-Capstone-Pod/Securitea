6f55e9153732d7e9820256c8bf78c749e537e867
erico07181 <erico@salesforce.com>
2022-08-04 14:40:08 -0700

    Added content and added more stylig to the module page

--- NEPL_Capstone/Frontend/src/hooks/useAuthenticationForm.jsx
+++ NEPL_Capstone/Frontend/src/hooks/useAuthenticationForm.jsx
@@ -2,72 +2,72 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const useAuthenticationForm = ({ user }) => {
-	const navigate = useNavigate();
-	const [errors, setErrors] = useState({});
-	const [form, setForm] = useState({
-		email: "",
-		first_name: "",
-		last_name: "",
-		password: "",
-		passwordConfirm: "",
-		title: "",
-		birthday: "",
-		isManager: false,
-	});
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState({});
+  const [form, setForm] = useState({
+    email: "",
+    first_name: "",
+    last_name: "",
+    password: "",
+    passwordConfirm: "",
+    title: "",
+    birthday: "",
+    isManager: false,
+  });
 
-	useEffect(() => {
-		if (user?.email) {
-			navigate("/UserDashboard");
-		}
-	}, [user, navigate]);
+  // useEffect(() => {
+  // 	if (user?.email) {
+  // 		navigate("/UserDashboard");
+  // 	}
+  // }, [user, navigate]);
 
-	const emailRegex = /[^@]+@[^@]+\.[^@]+/;
+  const emailRegex = /[^@]+@[^@]+\.[^@]+/;
 
-	function validateEmail(email) {
-		return String(email).toLowerCase().match(emailRegex);
-	}
+  function validateEmail(email) {
+    return String(email).toLowerCase().match(emailRegex);
+  }
 
-	const handleOnInputChange = (event) => {
-		if (event.target?.name == "isManagerName") {
-			setForm((f) => ({ ...f, isManager: event.target.checked }));
+  const handleOnInputChange = (event) => {
+    if (event.target?.name == "isManagerName") {
+      setForm((f) => ({ ...f, isManager: event.target.checked }));
 
-			//Resets the fields that are exclusive to a type of user
-			if (!event.target.checked) setForm((f) => ({ ...f, ["company"]: "" })); //If user, reset company
-			if (event.target.checked) setForm((f) => ({ ...f, ["token"]: "" })); //If manager, reset token
-		}
+      //Resets the fields that are exclusive to a type of user
+      if (!event.target.checked) setForm((f) => ({ ...f, ["company"]: "" })); //If user, reset company
+      if (event.target.checked) setForm((f) => ({ ...f, ["token"]: "" })); //If manager, reset token
+    }
 
-		if (event.constructor.name == "Date") {
-			setForm((f) => ({ ...f, birthday: new Date(event) }));
-		}
+    if (event.constructor.name == "Date") {
+      setForm((f) => ({ ...f, birthday: new Date(event) }));
+    }
 
-		if (event.target?.name === "email") {
-			if (!validateEmail(event.target.value)) {
-				setErrors((e) => ({ ...e, email: "Please enter a valid email." }));
-			} else {
-				delete errors.email;
-			}
-		}
-		if (
-			event.target?.name === "password" ||
-			event.target?.name === "passwordConfirm"
-		) {
-			if (event.target.value !== form.password) {
-				setErrors((e) => ({
-					...e,
-					passwordConfirm: "Passwords do not match.",
-				}));
-			} else {
-				delete errors.passwordConfirm;
-			}
-		}
+    if (event.target?.name === "email") {
+      if (!validateEmail(event.target.value)) {
+        setErrors((e) => ({ ...e, email: "Please enter a valid email." }));
+      } else {
+        delete errors.email;
+      }
+    }
+    if (
+      event.target?.name === "password" ||
+      event.target?.name === "passwordConfirm"
+    ) {
+      if (event.target.value !== form.password) {
+        setErrors((e) => ({
+          ...e,
+          passwordConfirm: "Passwords do not match.",
+        }));
+      } else {
+        delete errors.passwordConfirm;
+      }
+    }
 
-		setForm((f) => ({ ...f, [event.target?.name]: event.target?.value }));
-	};
+    setForm((f) => ({ ...f, [event.target?.name]: event.target?.value }));
+  };
 
-	return {
-		form,
-		errors,
-		setErrors,
-		handleOnInputChange,
-	};
+  return {
+    form,
+    errors,
+    setErrors,
+    handleOnInputChange,
+  };
 };
