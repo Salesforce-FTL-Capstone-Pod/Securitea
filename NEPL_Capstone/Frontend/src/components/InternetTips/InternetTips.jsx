f70e1766e2b4b98e055fd1abd3ecf215ec434f37
Lucas Ribeiro Vidal <lribeirovidal@salesforce.com>
2022-08-12 11:15:51 -0700

    progress for tips

--- NEPL_Capstone/Frontend/src/components/InternetTips/InternetTips.jsx
+++ NEPL_Capstone/Frontend/src/components/InternetTips/InternetTips.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import {
-  Button,
-  Grid,
-  Container,
-  Box,
-  AppBar,
-  Toolbar,
-  Typography,
-  Stack,
-  colors,
-  TextField,
-  InputAdornment,
-  IconButton,
-  Radio,
-  FormControlLabel,
-  FormControl,
-  RadioGroup,
+	Button,
+	Grid,
+	Container,
+	Box,
+	AppBar,
+	Toolbar,
+	Typography,
+	Stack,
+	colors,
+	TextField,
+	InputAdornment,
+	IconButton,
+	Radio,
+	FormControlLabel,
+	FormControl,
+	RadioGroup,
 } from "@mui/material";
 import * as color from "../../assets/colorPalette";
 import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
@@ -33,15 +33,17 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { Spacer } from "@nextui-org/react";
+import { useAuthContext } from "../../contexts/auth";
+import API from "../../services/apiClient";
 
 export default function TipsPage() {
-  return (
-    <>
-      <Navbar />
-      <InternetTips />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<InternetTips />
+			<Footer />
+		</>
+	);
 }
 
 // const speak = (evt) => {
@@ -50,129 +52,136 @@ export default function TipsPage() {
 // };
 
 export function InternetTips() {
-  const { form, errors, isProcessing, handleOnInputChange, handleOnSubmit } =
-    useLoginForm();
+	const { form, errors, isProcessing, handleOnInputChange, handleOnSubmit } =
+		useLoginForm();
 
-  const [isRight, setIsRight] = useState();
+	const [isRight, setIsRight] = useState();
 
-  const [answer, setAnswer] = useState("");
-  const [successMessage, setSuccessMessage] = useState("");
+	const [answer, setAnswer] = useState("");
+	const [successMessage, setSuccessMessage] = useState("");
+	const { user, setUser } = useAuthContext();
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    console.log("Answer", answer);
-  }, [answer]);
+	useEffect(() => {
+		console.log("Answer", answer);
+	}, [answer]);
 
-  function handleSubmit() {
-    if (answer === "right") {
-      setIsRight(true);
-      setSuccessMessage("");
-    } else if (answer === "wrong") {
-      setIsRight(false);
-      setSuccessMessage("Try Again!");
-    }
-  }
+	function handleSubmit() {
+		if (answer === "right") {
+			setIsRight(true);
+			setSuccessMessage("");
+		} else if (answer === "wrong") {
+			setIsRight(false);
+			setSuccessMessage("Try Again!");
+		}
+	}
 
-  return (
-    <Container
-      maxWidth={false}
-      style={{
-        display: "flex ",
-        flexDirection: "column",
-        backgroundColor: color.platinum,
-        height: "100%",
-        width: "75%",
-        marginTop: "100px",
-        marginBottom: "100px",
-      }}
-    >
-      <h1>Internet Tips</h1>
-      <p>
-        The best way to keep your companies and your personal data secure is by
-        following these following tips:
-      </p>
-      <Container>
-        <li>Do not step away from your workstation without locking it</li>
-        <li>Make sure websites you visit have "https" and a lock beside it</li>
-        <li>NEVER open weird or out of the ordinary links and attachments</li>
-        <li>Create strong passwords with the next steps</li>
-      </Container>
+	const nextHandler = async () => {
+		const respo = await API.addProgress("2");
+		setUser({ ...user, refresh: true });
+		delete user.refresh;
 
-      <Container
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          flexDirection: "column",
-        }}
-      >
-        <FormControl>
-          <RadioGroup>
-            <Container
-              style={{
-                opacity: "0.8",
-                justifyContent: "center",
-              }}
-            >
-              <img src={right} width={"400vh"} height={"50vh"} />
-            </Container>
-            <FormControlLabel
-              control={<Radio onChange={() => setAnswer("right")} />}
-              value="right"
-            />
+		navigate("../PasswordPage");
+	};
 
-            <Container style={{ width: "25%" }}></Container>
+	return (
+		<Container
+			maxWidth={false}
+			style={{
+				display: "flex ",
+				flexDirection: "column",
+				backgroundColor: color.platinum,
+				height: "100%",
+				width: "75%",
+				marginTop: "100px",
+				marginBottom: "100px",
+			}}
+		>
+			<h1>Internet Tips</h1>
+			<p>
+				The best way to keep your companies and your personal data secure is by
+				following these following tips:
+			</p>
+			<Container>
+				<li>Do not step away from your workstation without locking it</li>
+				<li>Make sure websites you visit have "https" and a lock beside it</li>
+				<li>NEVER open weird or out of the ordinary links and attachments</li>
+				<li>Create strong passwords with the next steps</li>
+			</Container>
 
-            <Container
-              style={{
-                opacity: "0.8",
-                justifyContent: "center",
-              }}
-            >
-              <img src={wrong} width={"400vh"} height={"50vh"} />
-            </Container>
-            <FormControlLabel
-              control={<Radio onChange={() => setAnswer("wrong")} />}
-              value="wrong"
-            />
-          </RadioGroup>
-        </FormControl>
+			<Container
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					flexDirection: "column",
+				}}
+			>
+				<FormControl>
+					<RadioGroup>
+						<Container
+							style={{
+								opacity: "0.8",
+								justifyContent: "center",
+							}}
+						>
+							<img src={right} width={"400vh"} height={"50vh"} />
+						</Container>
+						<FormControlLabel
+							control={<Radio onChange={() => setAnswer("right")} />}
+							value="right"
+						/>
 
-        <Button onClick={handleSubmit}>Submit</Button>
-      </Container>
+						<Container style={{ width: "25%" }}></Container>
 
-      <br></br>
-      <br></br>
+						<Container
+							style={{
+								opacity: "0.8",
+								justifyContent: "center",
+							}}
+						>
+							<img src={wrong} width={"400vh"} height={"50vh"} />
+						</Container>
+						<FormControlLabel
+							control={<Radio onChange={() => setAnswer("wrong")} />}
+							value="wrong"
+						/>
+					</RadioGroup>
+				</FormControl>
 
-      {isRight ? (
-        <>
-          <Container
-            maxWidth={false}
-            style={{
-              background: "#C1E1C1",
-              justifyContent: "center",
-              alignItems: "center",
-              display: "flex",
-              flexWrap: "row",
-              height: "100%",
-              width: "100%",
-            }}
-          >
-            <Typography>
-              Correct! Please click the button to continue!
-            </Typography>
-            <Spacer />
+				<Button onClick={handleSubmit}>Submit</Button>
+			</Container>
 
-            <Button>
-              <Link to="/PasswordPage">
-                <ArrowCircleRightIcon />
-              </Link>
-            </Button>
-          </Container>
-        </>
-      ) : (
-        <Typography>{successMessage}</Typography>
-      )}
-    </Container>
-  );
+			<br></br>
+			<br></br>
+
+			{isRight ? (
+				<>
+					<Container
+						maxWidth={false}
+						style={{
+							background: "#C1E1C1",
+							justifyContent: "center",
+							alignItems: "center",
+							display: "flex",
+							flexWrap: "row",
+							height: "100%",
+							width: "100%",
+						}}
+					>
+						<Typography>
+							Correct! Please click the button to continue!
+						</Typography>
+						<Spacer />
+
+						<Button onClick={nextHandler}>
+							<ArrowCircleRightIcon />
+						</Button>
+					</Container>
+				</>
+			) : (
+				<Typography>{successMessage}</Typography>
+			)}
+		</Container>
+	);
 }
